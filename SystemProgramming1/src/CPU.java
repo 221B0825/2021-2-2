e42b3da99668b0d02aedc9f227af1c069ce8d151
221B0825 <silverwest0825@gmail.com>
2022-02-11 06:56:47 +0900

    2021-09-29

--- SystemProgramming1/src/CPU.java
+++ SystemProgramming1/src/CPU.java
@@ -1,101 +1,165 @@
 
 public class CPU {
 	// declaration
-	private enum ERegisters{
-		ePC,
-		eSP,
-		eAC,
-		eIR,
-		eStatus,
-		eMar,
-		embr
+	private enum ERegisters {
+		ePC, eSP, eAC, eIR, eStatus, eMAR, eMBR
 	}
-	
+
 	private enum EOpCode {
-		eHalt,
-		eLDA,
-		eADD,
-		eSub,
-		eBEq,//브랜치이퀄
-		eBGT,//브랜치 그래이터 댄
-		eBranch //그냥 그대로 가라
-		
+		eHalt, eLDA, eSTA, eAdd, eSub, eEQ, eGT, eBEq, eBGT, eBranch
+
 	}
-	private class Register{
+
+	private class CU {
+		// PC를 제어
+	}
+
+	private class ALU {
+
+		public void add() {
+
+		}
+
+		public void subtract() {
+			
+		}
+
+		public void equal() {
+			
+		}
+
+		public void greaterThan() {
+			
+		}
+	}
+
+	private class Register {
 		protected short value;
+
 		public short getValue() {
 			return this.value;
 		}
+
 		public void setValue(short value) {
 			this.value = value;
 		}
 	}
-	
-	class IR extends Register{
-		public short getOpcode() {
-			return (short)(this.value >> 8);
+
+	class IR extends Register {
+		public short getOpCode() {
+			return (short) (this.value >> 8);
 		}
-		public short getOprand() {
-			return (short)(this.value & 0x00FF);
+
+		public short getOperand() {
+			return (short) (this.value & 0x00FF);
 		}
 	}
-	
-	private class ALU{
-		//계산하라고 오피코드를 주면 계산할거고
-	}
-	private class CU{
-		// PC를 제어
-	}
-	
+
 	// components
-	private int registers[];
-	private IR ir;
-	private ALU alu;
 	private CU cu;
-	
+	private ALU alu;
+
+	private Register registers[];
+	// associations
+	private Memory memory;
 	// status
 	private boolean bPowerOn;
+
 	private boolean isPowerOn() {
 		return this.bPowerOn;
 	}
+
 	public void setPowerOn() {
 		this.bPowerOn = true;
 		this.run();
 	}
+
 	public void shutDown() {
 		this.bPowerOn = false;
 	}
-	
+
 	// constructor
 	public CPU() {
-		this.registers = new int[ERegisters.values().length];
-		this.alu = new ALU();
 		this.cu = new CU();
+		this.alu = new ALU();
+		this.registers = new Register[ERegisters.values().length];
 	}
-	private void fetch() {
 
-		
+	public void associate(Memory memory) {
+		this.memory = memory;
 	}
-	private void decode() {
-			
-		}
+
+	private void fetch() {
+		// PC --> MAR
+		this.registers[ERegisters.eMAR.ordinal()].setValue(this.registers[ERegisters.ePC.ordinal()].getValue());
+		this.memory.load(); // MAR과 MBR을 연결해야 함
+		this.registers[ERegisters.eIR.ordinal()].setValue(this.registers[ERegisters.eMBR.ordinal()].getValue());
+	}
+
+	private void load() {
+		this.registers[ERegisters.eMAR.ordinal()]
+				.setValue((short) (((IR) this.registers[ERegisters.eIR.ordinal()]).getOperand()
+						+ this.registers[ERegisters.eSP.ordinal()].getValue()));
+
+		this.memory.load();
+	}
+
 	private void execute() {
-		switch(ir.getOpCode()) {
+		switch (EOpCode.values()[((IR) this.registers[ERegisters.eIR.ordinal()]).getOpCode()]) {
 		case eHalt:
 			break;
-			
+		case eLDA:
+			this.load();
+			break;
+		case eSTA:
+			break;
+		case eAdd:
+			this.registers[ERegisters.eAC.ordinal()].setValue(this.registers[ERegisters.eMBR.ordinal()].getValue());
+			this.load();
+			this.alu.add();
+			break;
+		case eSub:
+			this.registers[ERegisters.eAC.ordinal()].setValue(this.registers[ERegisters.eMBR.ordinal()].getValue());
+			this.load();
+			this.alu.subtract();
+			break;
+		case eEQ: //A = B
+			this.registers[ERegisters.eAC.ordinal()].setValue(this.registers[ERegisters.eMBR.ordinal()].getValue());
+			this.load();
+			this.alu.equal(); //결과값이 status에 저장됨
+			break;
+		case eGT:
+			this.registers[ERegisters.eAC.ordinal()].setValue(this.registers[ERegisters.eMBR.ordinal()].getValue());
+			this.load();
+			this.alu.greaterThan();
+			break;
+		case eBEq:
+			break;
+		case eBGT:
+			break;
+		case eBranch:
+			break;
+
 		}
+
 	}
+
 	private void checkInterrupt() {
-		
+
 	}
+
 	public void run() {
-		while(this.isPowerOn()) {
+		while (this.isPowerOn()) {
 			this.fetch();
-			this.decode();
 			this.execute();
 			this.checkInterrupt();
 		}
 	}
-	
+
+	public static void main(String[] args) {
+		CPU cpu = new CPU();
+		Memory memory = new Memory();
+		cpu.associate(memory);
+		cpu.setPowerOn();
+	}
 }
--- SystemProgramming1/src/Memory.java
+++ SystemProgramming1/src/Memory.java
@@ -1,4 +1,8 @@
 
 public class Memory {
 
+	public void load() {
+		
+	}
+
 }
