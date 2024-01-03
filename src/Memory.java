a0bb79fe83ffaea2e03366b125097555b47ad05a
221B0825 <eunseoyu0825@gmail.com>
2024-01-03 18:09:44 +0900

    feat: 2021-10-04

--- src/Memory.java
+++ src/Memory.java
@@ -1,48 +1,23 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 
 public class Memory {
-	private short[] segment;
-	private short segmentSize;
-	
-	public Memory() throws IOException {
-		this.segmentSize = 10;
-		this.segment = new short[segmentSize];
-		int index = 0;
-		
-		BufferedReader bufferedReader = new BufferedReader(new FileReader("./exe/exe1"));
-		while(bufferedReader.ready()) {
-			String line = bufferedReader.readLine();
-			if(line.equals(""))
-				break;
-			else {
-				this.segment[index] = Short.parseShort(line,2);
-				System.out.println(segment[index]);
-				index++;
-			}
-		}
-	}
-	// header setting
-	public short setPC() {
-		return (short) (this.segment[0]+5);
+	public class Process {
+		private short header[] = { 
+				0x001E,
+				0x0010,
+				0x0008
+		};
+		private short codeSegment[] = {
+				0x0003, //SetAC 3
+				0x0200 //STA A
+		};
+		private short dataSegment[] = {
+				0x0000,
+				0x0000
+		};
 	}
 
-	public short setSP() {
-		return this.segment[1];
+	public void load() {
+
 	}
 
-	public void load(short value) throws FileNotFoundException {
-		System.out.println(value);
-	}
 }
-//BufferedReader exe1 = new BufferedReader(new FileReader("./exe/exe1"));
-//this.segment = new ArrayList<Integer>();
-//while (exe1.ready()) {
-//	String line = exe1.readLine();
-//	if (!line.equals("")) {
-//		segment.add(line);
-//	}
-//}
-//exe1.close();
